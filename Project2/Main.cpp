de1113f1596685a6a72244369e361076136bf862
William Shepherd <williamrshepherd@gmail.com>
2016-02-03 17:13:57 -0600

    Revert "Reading from stin"
    This reverts commit ba33324cf03f6ac934dd49cdfbbe022261cc5291.

--- Project2/Main.cpp
+++ Project2/Main.cpp
@@ -5,16 +5,11 @@ using namespace std;
 
 int main()
 {
-	string input;
-	cout << "Enter expression: ";
-	cin >> input;
-
-	cout << ExpressionNotationConverter::ConvertInfixToPostFix(input) << endl;
-	//cout << ExpressionNotationConverter::ConvertInfixToPostfix("3+7") << endl;
-	//cout << ExpressionNotationConverter::ConvertInfixToPostfix("3+7*2") << endl;
-	//cout << ExpressionNotationConverter::ConvertInfixToPostfix("(3+7)*2") << endl;
-	//cout << ExpressionNotationConverter::ConvertInfixToPostfix("3+(7-2)*((5-8)") << endl;
-	//cout << ExpressionNotationConverter::ConvertInfixToPostfix("3+((7*(2-4)+5)*2+9)*(4-8)-10") << endl;
+	cout << ExpressionNotationConverter::ConvertInfixToPostfix("3+7") << endl;
+	cout << ExpressionNotationConverter::ConvertInfixToPostfix("3+7*2") << endl;
+	cout << ExpressionNotationConverter::ConvertInfixToPostfix("(3+7)*2") << endl;
+	cout << ExpressionNotationConverter::ConvertInfixToPostfix("3+(7-2)*((5-8)") << endl;
+	cout << ExpressionNotationConverter::ConvertInfixToPostfix("3+((7*(2-4)+5)*2+9)*(4-8)-10") << endl;
 
 	cin.get();
 
