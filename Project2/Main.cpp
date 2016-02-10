aa3d351be41bb2a701da65750f6d9fc8445f6629
William Shepherd <williamrshepherd@gmail.com>
2016-02-10 16:27:45 -0600

    Changed project 2 to read input from a file

--- Project2/Main.cpp
+++ Project2/Main.cpp
@@ -1,15 +1,21 @@
 #include <iostream>
+#include <sstream>
+#include <fstream>
 #include "ExpressionNotationConverter.h"
 
 using namespace std;
 
 int main()
 {
-	cout << ExpressionNotationConverter::ConvertInfixToPostfix("3+7") << endl;
-	cout << ExpressionNotationConverter::ConvertInfixToPostfix("3+7*2") << endl;
-	cout << ExpressionNotationConverter::ConvertInfixToPostfix("(3+7)*2") << endl;
-	cout << ExpressionNotationConverter::ConvertInfixToPostfix("3+(7-2)*((5-8)") << endl;
-	cout << ExpressionNotationConverter::ConvertInfixToPostfix("3+((7*(2-4)+5)*2+9)*(4-8)-10") << endl;
+	string filename = "C:\\infix.txt";
+	ifstream fs(filename);
+	string line;
+
+	while (!fs.eof())
+	{
+		fs >> line;
+		cout << ExpressionNotationConverter::ConvertInfixToPostfix(line) << endl;
+	}
 
 	cin.get();
 
