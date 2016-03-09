6bc9077591f51a9b9b457c5faa9c047cafb3da4c
William Shepherd <williamrshepherd@gmail.com>
2016-03-09 16:58:11 -0600

    Completed BST interface

--- Project5/BST/BST/BinarySearchTree.hpp
+++ Project5/BST/BST/BinarySearchTree.hpp
@@ -10,18 +10,60 @@
 #define BinarySearchTree_hpp
 
 #include <stdio.h>
+#include <string>
+
+using std::string;
 
 #endif /* BinarySearchTree_hpp */
 
+#define MAX_NODES 100
+
+enum TraversalType {
+    Inorder,
+    Preorder,
+    Postorder
+};
+
 struct BinaryTreeNode {
     int data;
-    
+    int leftndex;
+    int rightIndex;
 };
 
 class BinarySearchTree {
+    
+private:
+    BinaryTreeNode nodeArray[MAX_NODES];
+    
+    string inorderTraversal();
+    string preorderTraversal();
+    string postOrderTraversal();
+    
 public:
     BinarySearchTree();
+    BinarySearchTree(const BinarySearchTree &obj);
     ~BinarySearchTree();
     
+    int Find(int value);
+    int FindMin();
+    int FindMax();
     void Insert(int data);
+    void Delete(int data);
+    
+    inline string Traversal(TraversalType traversal) {
+        switch (traversal) {
+            case Inorder:
+                return inorderTraversal();
+                break;
+            case Preorder:
+                return preorderTraversal();
+                break;
+            case Postorder:
+                return postOrderTraversal();
+                break;
+            default:
+                return "";
+                break;
+        }
+    };
 };
